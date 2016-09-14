8b28c0453358e861141957c59d89aad8015d300f
dashingshakeel <shkeelrauf@gmail.com>
2016-09-14 14:56:26 +0500

    Add controller,view and rout to index letter

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,9 +1,9 @@
-class ApplicationController < ActionController::Base
+class LettersController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def index
-    haml :index
+    @letters=Letter.all
   end
 
-  protect_from_forgery with: :exception
+  
 end
